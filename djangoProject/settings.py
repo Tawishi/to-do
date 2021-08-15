f6a30b073e2ab0254ba9642694d0f80a7f417b5f
Tawishi <tawishisharma1@gmail.com>
2021-08-15 14:43:45 +0530

    Changes settings

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['django-personal-todo-app.herokuapp.com',]
+ALLOWED_HOSTS = ['127.0.0.1','django-personal-todo-app.herokuapp.com',]
 
 
 # Application definition
